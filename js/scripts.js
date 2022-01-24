9c0d08c46a1997189f69632de850788b1b369333
Riley Shimp <rilryshimp@gmail.com>
2022-01-24 13:58:34 -0800

    Add all UI logic

--- js/scripts.js
+++ js/scripts.js
@@ -2,17 +2,17 @@
 function AddressBook() {
   this.contacts = {};
   this.currentId = 0;
-}
+};
 
 AddressBook.prototype.addContact = function(contact) {
   contact.id = this.assignId();
   this.contacts[contact.id] = contact;
-}
+};
 
 AddressBook.prototype.assignId = function() {
   this.currentId += 1;
   return this.currentId;
-}
+};
 
 AddressBook.prototype.findContact = function(id) {
   if (this.contacts[id] != undefined) {
@@ -35,22 +35,64 @@ function Contact(firstName, lastName, phoneNumber) {
   this.firstName = firstName;
   this.lastName = lastName;
   this.phoneNumber = phoneNumber;
-}
+};
 
 
 Contact.prototype.fullName = function () {
   return this.firstName + " " + this.lastName;
+};
+
+
+// User Interface Logic ---------
+let addressBook = new AddressBook();
+
+function displayContactDetails(addressBookToDisplay) {
+  let contactsList = $("ul#contacts");
+  let htmlForContactInfo = "";
+  Object.keys(addressBookToDisplay.contacts).forEach(function(key){
+    const contact = addressBookToDisplay.findContact(key);
+    htmlForContactInfo += "<li id=" + contact.id + ">" + contact.firstName + " " + contact.lastName + "</li>";
+  });
+  contactsList.html(htmlForContactInfo);
+};
+
+function showContact(contactId) {
+  const contact = addressBook.findContact(contactId);
+  $("#show-contact").show();
+  $(".first-name").html(contact.firstName);
+  $(".last-name").html(contact.lastName);
+  $(".phone-number").html(contact.phoneNumber);
+  let buttons = $("#buttons");
+  buttons.empty();
+  buttons.append("<button class ='deleteButton' id=" + contact.id + ">Delete</button>");
 }
 
+function attachContactListeners() {
+  $("ul#contacts").on("click", "li", function() {
+    showContact(this.id);
+  });
+  $("#buttons").on("click", ".deleteButton", function () {
+    addressBook.deleteContact(this.id);
+    $("#show-contact").hide();
+    displayContactDetails(addressBook);
+  });
+};
 
+$(document).ready(function() {
+  attachContactListeners();
+  $("form#new-contact").submit(function(event) {
+    event.preventDefault();
+    const inputtedFirstName = $("input#new-first-name").val();
+    const inputtedLastName = $("input#new-last-name").val();
+    const inputtedPhoneNumber = $("input#new-phone-number").val();
 
-// to test in console
-let testContact = new Contact("Ada", "Lovelace", "503-555-1111");
-let testContact2 = new Contact("Grace", "Hopper", "503-555-0199");
-let addressBook = new AddressBook();
-addressBook.addContact(testContact);
-addressBook.addContact(testContact2);
-// then check your address book by calling the "addressBook" variable
-// to check all of their info  -- addressBook.testContact["Ada"]
-// to find off of the ID in address book -- addressBook.findContact(1)
-// to delete contacts -- addressBook.deleteContact(1)
+    $("input#new-first-name").val("");
+    $("input#new-last-name").val("");
+    $("input#new-phone-number").val("");
+
+    let newContact = new Contact(inputtedFirstName, inputtedLastName, inputtedPhoneNumber);
+    addressBook.addContact(newContact);
+    displayContactDetails(addressBook);
+  });
+});
+
